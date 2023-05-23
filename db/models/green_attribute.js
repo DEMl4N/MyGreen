5e95dd44bcae03ef9500c30a2d6e80274ed6fb9f
yunsangee <michael0622@naver.com>
2023-05-23 23:16:50 +0900

    iot+

--- db/models/green_attribute.js
+++ db/models/green_attribute.js
@@ -1,16 +1,14 @@
-const db = require('../database')
+const db = require('../database');
 
 const attributeSchema = new db.Schema({
     optimalTemperature: {
-        type: Number,   // 최적 온도: iot에서 +- 10% 로 맞추면 될 듯
+        type: Number,
         required: true
     },
-
     wateringCycle: {
-        type: Number,   //  화분에 물주는 주기 (일)
+        type: Number,
         required: true
     }
-
 });
 
-module.exports = attributeSchema
+module.exports = attributeSchema;
--- routes/iot.js
+++ routes/iot.js
@@ -1,12 +1,25 @@
 const express = require('express')
+const attributeSchema = require('../db/models/green_attribute')
+const db = require('../db/database')
 
-var router = express.Router()
+const router = express.Router()
+const Attribute = db.model('Attribute', attributeSchema)
 
 router.post('/', (req, res) => {
-    console.log(req.body.lux, req.body.humidity, req.body.temperature)
-    res.json({
-        status: "OK"
-    })
+  console.log(req.body.lux, req.body.humidity, req.body.temperature)
+  res.json({
+    status: "OK"
+  })
+})
+
+router.get('/', async (req, res) => {
+  try {
+    const attributes = await Attribute.findOne({})
+    const { optimalTemperature, wateringCycle } = attributes
+    res.json({ optimalTemperature, wateringCycle })
+  } catch (err) {
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
 })
 
 module.exports = router

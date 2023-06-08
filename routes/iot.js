82981c1626b7d05afc1d40fb9d7b9671b39c9fc1
yunsangee <michael0622@naver.com>
2023-06-08 16:42:24 +0900

    FCM

--- routes/iot.js
+++ routes/iot.js
@@ -1,39 +1,63 @@
 const express = require('express');
 const db = require('../db/database');
+const admin = require('firebase-admin');
+const serviceAccount = require('C:\\Users\\micha\\Desktop\\serviceAccountKey.json');
+const app = require('../app');
+
+
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: 'your-storage-bucket.appspot.com'
+});
+
+const bucket = admin.storage().bucket();
 
 const router = express.Router();
 const Green = require('./green');
 
-const criteria = {
-  humidity: {
-    max: 80,
-    min: 40,
-  },
-  temperature: {
-    max: 30,
-    min: 10,
-  },
-};
+router.post('/', async (req, res) => {
+  const { id, status } = req.body;
 
-router.post('/', (req, res) => {
-  const { id, status, humidity, temperature } = req.body;
-
-  console.log(id, status, humidity, temperature);
-  res.json({
-    status: 'OK',
-  });
-
-  if (humidity > criteria.humidity.max) {
-    console.log('Warning: 습도가 기준치를 초과하였습니다.');
-  } else if (humidity < criteria.humidity.min) {
-    console.log('Warning: 습도가 기준치 미달하였습니다.');
+  // 예외 처리: id나 status가 누락된 경우
+  if (!id || !status) {
+    return res.status(400).json({ error: 'id and status are required' });
   }
 
-  if (temperature > criteria.temperature.max) {
-    console.log('Warning: 온도가 기준치를 초과하였습니다.');
-  } else if (temperature < criteria.temperature.min) {
-    console.log('Warning: 온도가 기준치 미달하였습니다.');
+  try {
+    const idExists = await Green.compareId(id);
+    if (idExists) {
+      res.status(200).json({ message: 'ID exists in the database' });
+    } else {
+      res.status(200).json({ message: 'ID does not exist in the database' });
+    }
+
+    // Firebase Storage에 status 데이터 업로드
+    const remoteFilePath = `status/${id}.json`;
+    const fileContents = JSON.stringify({ status });
+
+    const file = bucket.file(remoteFilePath);
+    const stream = file.createWriteStream({
+      metadata: {
+        contentType: 'application/json'
+      }
+    });
+
+    stream.on('error', (error) => {
+      console.error('Error uploading file:', error);
+      res.status(500).json({ error: 'Failed to upload status data' });
+    });
+
+    stream.on('finish', () => {
+      console.log('Status data uploaded successfully.');
+      // res.status(200).json({ message: 'Status data uploaded successfully' });
+    });
+
+    stream.end(fileContents);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to compare id' });
   }
 });
+
 
 module.exports = router;
---
+++ utilities/firebaseRouter.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const admin = require('firebase-admin');
+const serviceAccount = require('C:\\Users\\micha\\Desktop\\serviceAccountKey.json');
+
+const router = express.Router();
+const Green = require('../db/models/green');
+
+router.get('/data', (req, res) => {
+  // Firebase logic to retrieve data
+  // Send response
+});
+
+router.post('/data', (req, res) => {
+  // Firebase logic to update data
+  // Send response
+});
+
+try {
+  if (!admin.apps.length) {
+    const firebaseApp = admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+      storageBucket: 'your-storage-bucket.appspot.com'
+    });
+
+    const bucket = firebaseApp.storage().bucket();
+  }
+} catch (error) {
+  console.error('Failed to initialize Firebase app:', error);
+}
+
+module.exports = router;
